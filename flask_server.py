8900e13128da55bd636d116d36fc0389df8624cc
Erik Schauer <do1ffe@darc.de>
2025-07-07 13:35:19 +0200

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -99,7 +99,7 @@ def _get_github_version():
 
 
 GITHUB_VERSION = _get_github_version()
-PROGRAM_VERSION = f'FT-991A-Remote 0.1.{GITHUB_VERSION}'
+PROGRAM_VERSION = f'FT-991A-Remote V0.1.{GITHUB_VERSION}'
 
 sock = Sock(app)
 
